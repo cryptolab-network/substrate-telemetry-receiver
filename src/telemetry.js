403f32530376764e749dba0c43d52b53b2cba321
yktseng <tanistseng@gmail.com>
2021-04-13 10:09:18 +0800

    update built code

--- src/telemetry.js
+++ src/telemetry.js
@@ -29,10 +29,12 @@ class Telemetry extends events_1.default.EventEmitter {
         return new Promise((resolve, reject) => {
             this.connection = new ws_1.default(this.url);
             this.connection.on('open', () => {
-                this.connection.send('subscribe:Kusama');
+                setTimeout(() => {
+                    this.connection.send('subscribe:Kusama');
+                }, 2000);
                 setTimeout(() => {
                     this.isStarting = false;
-                }, 10000);
+                }, 12000);
                 setInterval(() => {
                     this.connection.send('ping:' + this.pingCount++);
                 }, 60000);
@@ -46,8 +48,14 @@ class Telemetry extends events_1.default.EventEmitter {
                 }
             });
             this.connection.on('error', (err) => {
+                var _a;
                 console.error(err);
-                reject(err);
+                (_a = this.connection) === null || _a === void 0 ? void 0 : _a.terminate();
+                this.emit('error');
+            });
+            this.connection.on('close', () => {
+                console.error('websocket connection closed');
+                this.emit('close');
             });
         });
     }
